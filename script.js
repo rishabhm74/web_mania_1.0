7b67f64a6f9dc560fde8184875c7097b582ca0d9
rishabhm74 <rishabhm1974@gmail.com>
2019-10-05 18:39:55 +0530

    uploading files

---
+++ script.js
@@ -0,0 +1,14 @@
+function show_address(){
+    document.getElementById("address").style.display = "block";
+    document.getElementById("phone").style.display = "block";
+    document.getElementById("about2_us").style.display = "block";
+    document.getElementById("active").style.border = "none";
+}
+function hide_address(){
+    document.getElementById("address").style.display = "none";
+    document.getElementById("phone").style.display = "none";
+    document.getElementById("about2_us").style.display = "none";
+}
+function testimonia(){
+    document.getElementById("main_container").style.backgroundImage = "url('closed.jpg')";
+}
